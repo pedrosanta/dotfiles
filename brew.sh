d85620376bcc515ea50d99cc5015dc82b92747b4
Pedro Machado Santa <pedro.santa@gmail.com>
2017-11-03 18:23:21 +0000

    Update messenger for desktop cask name

--- brew.sh
+++ brew.sh
@@ -38,7 +38,7 @@ brew cask install docker
 brew cask install sourcetree
 brew cask install kitematic
 brew cask install steam
-brew cask install messenger-for-desktop
+brew cask install aluxian-messenger
 brew cask install skype # Meh.
 brew cask install typora
 brew cask install duet
