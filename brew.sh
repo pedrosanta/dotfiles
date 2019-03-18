3c80c13c619b6f0ab9a9b9fcd15f77bc933575c9
Pedro Machado Santa <pedro.santa@gmail.com>
2019-03-18 00:21:50 +0000

    Remove ST obsolete git gutter package

--- brew.sh
+++ brew.sh
@@ -22,7 +22,6 @@ brew tap homebrew/cask-versions
 # Install binary Caskroom 'casks'
 ## System Essentials
 brew cask install itsycal
-#brew cask install flux # Disabling this for now, as I've been using macOS native Night Shift
 brew cask install skyfonts
 brew cask install java # Meh.
 ## Other
